563b1bb30d3e1bbe731df1b3c75d346d45b79408
Youssef Medhat <yossefmedhat432@gmail.com>
2026-06-04 08:53:07 +0200

    fixing deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -11,6 +12,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+    nitro(),
     viteReact(),
   ],
 });
